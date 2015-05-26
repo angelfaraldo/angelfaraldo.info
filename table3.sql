42114acf966a19df53863fef2f258cac0f509b8c
Ángel Faraldo <angelfaraldo@gmail.com>
2015-05-26 17:39:30 +0200

    cleanup

--- table3.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `EVENTS` (`lang`, `fecha`, `evento`, `venue`, `town`) VALUES
-('EN', '2013-06-24', 'Improv Set with Frank Mauceri and Victoria Gibson', 'Oorsprong Curator Series', 'Amsterdam'),
-('EN', '2013-06-21', 'Improv Set with Martijn Barendreght', 'Institute of Sonology', 'The Hague'),
-('EN', '2013-06-19', 'Live-Electronics and Sound-Difussion with Vertixe Sonora Ensemble. Works by Pierre Jodlowsky and Arturo Fuentes', 'Ciclo do Audible, Casino', 'Ourense'),
-('EN', '2013-06-13', 'Live-Electronics and Sound-Difussion with Vertixe Sonora Ensemble. Works by Junghae Lee, Luis Antunes and Germán Alonso', 'Normal', 'A Coruña'),
-('EN', '2013-06-02', 'Live-Electronics and Sound-Difussion with Vertixe Sonora Ensemble. Works by Peter Ablinger, Arturo Fuentes and J.M. Cabeza de Vaca', 'Ciclo do Audible, Museo Municipal', 'A Coruña'),
-('EN', '2013-05-30', 'The White Noise Orchestra', 'Wonderwerp, Studio Loos', 'The Hague'),
-('EN', '2013-05-10', 'Resindency and Performances of City Sondols: Toronto, in collaboration with Matteo Marangoni', 'Deep Wireless Festival and TransX Symposium, various locations (until May 20th)', 'Toronto'),
